7319167a6509557e7ac0e132aba9bfd9942c3ef2
Eric Lemes <eric_efl@hotmail.com>
2021-08-25 16:32:25 -0300

    feat: ensure SignUpController return an erro if no email is provided

--- src/presentation/controllers/singnup.spec.ts
+++ src/presentation/controllers/singnup.spec.ts
@@ -1,5 +1,5 @@
 
-import {SignUpController} from './singnup'
+import { SignUpController } from './singnup'
 
 describe('SingUp Controller', () => {
   test('Should retrun 400 if no name is provided', () => {
@@ -14,5 +14,19 @@ describe('SingUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing Param: name'))
+  })
+
+  test('Should retrun 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing Param: email'))
   })
 })
