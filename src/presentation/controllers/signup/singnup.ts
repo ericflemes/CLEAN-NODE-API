8ece68bbda6cfe6ed671ebbf1f2d9592b3cc6237
Eric Lemes <eric_efl@hotmail.com>
2021-08-30 11:53:49 -0300

    refactor: set http response

--- src/presentation/controllers/signup/singnup.ts
+++ src/presentation/controllers/signup/singnup.ts
@@ -1,4 +1,4 @@
-import { HttpRequest, EmailValidator, Controller, AddAccount } from './signup-protocols'
+import { HttpRequest, HttpResponse, EmailValidator, Controller, AddAccount } from './signup-protocols'
 import { MissingParamError, InvalidParamError } from '../../erros'
 import { badRequest, serverError, ok } from '../../helper/http-helper'
 
@@ -11,7 +11,7 @@ export class SignUpController implements Controller {
     this.addAccount = addAccount
   }
 
-  handle (httpRequest: HttpRequest): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     try{
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
